9a9160acc0643399ba7b34a861c205a326a87e20
David Barbato <daviddev1306@gmail.com>
2022-05-27 12:00:08 -0300

    created component header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
+    <app-header />
     
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
 
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    AppHeader
+  }
 }
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css' /* Arquivo de estilo do bootstrap */
 import App from './App.vue'
 
 Vue.config.productionTip = false
