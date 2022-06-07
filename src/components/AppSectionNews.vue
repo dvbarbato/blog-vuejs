449011fa1d885eb108e674c17eb9c0c50521ee49
David Barbato <daviddev1306@gmail.com>
2022-06-07 16:16:52 -0300

    loading news with  Getters

--- src/components/AppSectionNews.vue
+++ src/components/AppSectionNews.vue
@@ -24,42 +24,22 @@
 
 <script>
 import AppSectionNewsIndividual from './AppSectionNewsIndividual.vue'
+import { mapGetters } from 'vuex'
+
 export default {
   components: {
       AppSectionNewsIndividual
     },
     data() {
         return {
-            news: []
+        
         }
     },
-    created() {
-        // Transformar o data em JSON
-        this.news = [{
-            id: 1,
-            title: 'Começam os treinos para a nova temporada',
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
-            date: '2021-01-04',
-            img: 'news1.jpg',
-            imgInfo: 'Notícia 1'
-        }, {
-            id: 2,
-            title: 'Jogo do Inter termina empatado',
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
-            date: '2021-01-05',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        }, {
-            id: 3,
-            title: 'Real vai inauguarar o novo estádio na sexta-feira',
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
-            date: '2021-01-09',
-            img: 'news3.jpg',
-            imgInfo: 'Notícia 3'
-
-        }]
+    computed: {
+        ...mapGetters({
+            news: 'getNews'
+        })
     }
-    
 }
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,30 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         championship: 'Campeonato Brasileiro',
-        clubName: 'Hcode Treinamento'
+        clubName: 'Hcode Treinamento',
+        news: [{
+            id: 1,
+            title: 'Começam os treinos para a nova temporada',
+            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
+            date: '2021-01-04',
+            img: 'news1.jpg',
+            imgInfo: 'Notícia 1'
+        }, {
+            id: 2,
+            title: 'Jogo do Inter termina empatado',
+            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
+            date: '2021-01-05',
+            img: 'news2.jpg',
+            imgInfo: 'Notícia 2'
+        }, {
+            id: 3,
+            title: 'Real vai inauguarar o novo estádio na sexta-feira',
+            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quidem, dolore ipsum quae fugit voluptatem natus consequatur ullam assumenda sit nesciunt odio veniam eligendi tempore possimus non maiores! Quisquam, nemo? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum iure consectetur voluptas odit, ea sed quos blanditiis facere, omnis maxime porro sint culpa cupiditate exercitationem corporis fuga facilis eum adipisci!',
+            date: '2021-01-09',
+            img: 'news3.jpg',
+            imgInfo: 'Notícia 3'
+
+        }]
     },
     getters: {
         getChampionship(state){
@@ -14,6 +37,9 @@ export default new Vuex.Store({
         },
         getClubName(state) {
             return state.clubName
+        },
+        getNews(state) {
+            return state.news
         }
     }
 })
